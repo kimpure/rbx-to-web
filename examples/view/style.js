a9939d847871704a64f57e113fc3b286219eb0ee
kimpure <gimleni593@gmail.com>
2025-05-15 10:52:54 +0000

    ui_corner.js is use not class use function and examples fix

--- examples/view/style.js
+++ examples/view/style.js
@@ -1,11 +1,4 @@
-class UiCorner {
-    constructor(class_name, offset, scale) {
-        this.el = document.querySelector('.' + class_name)
-        this.offset = offset;
-        this.scale = scale;
-    }
-
-    init() {
-        this.el.style.borderRadius = `${this.el.offsetHeight * this.scale + this.offset}px`;
-    }
+function applyUiCornerStyle(class_name, offset, scale) {
+    const el = document.querySelector('.' + class_name)
+    el.style.borderRadius = `${el.offsetHeight * scale + offset}px`;
 }
